793b9d386fcc76466391c9e3897abac650f6aa5a
Sandesh-Jamdhade <sandeshjamdhade26@gmail.com>
2026-02-03 01:19:58 +0530

    URLs of APIs that has to be run

--- employees/urls.py
+++ employees/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('employees/', views.employee_list),
     path('employees/create/', views.employee_create),
+    path('employees/<int:id>/', views.employee_detail),
+    path('employees/<int:id>/update/', views.employee_update),
+    path('employees/<int:id>/delete/', views.employee_delete),
 ]
