06a7c6baec72f96338c5c0665736148c1727aff1
Sandesh-Jamdhade <sandeshjamdhade26@gmail.com>
2026-02-03 13:31:46 +0530

    Added APIs urls

--- employees/urls.py
+++ employees/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('employees/', views.employee_list),
-    path('employees/create/', views.employee_create),
-    path('employees/<int:id>/', views.employee_detail),
-    path('employees/<int:id>/update/', views.employee_update),
-    path('employees/<int:id>/delete/', views.employee_delete),
+    path("employees/", views.employee_list),
+    path("employees/create/", views.employee_create),
+    path("employees/<int:id>/", views.employee_detail),
+    path("employees/update/<int:id>/", views.employee_update),
+    path("employees/delete/<int:id>/", views.employee_delete),
+    path("login/", views.login_api),
+    path("register/", views.register_api),
+    path("is-admin/", views.is_admin),
 ]
