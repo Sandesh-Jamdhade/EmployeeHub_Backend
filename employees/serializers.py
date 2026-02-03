3065435af2d5376205fbd6781faaeaf7f18f471f
Sandesh-Jamdhade <sandeshjamdhade26@gmail.com>
2026-02-03 13:31:10 +0530

    Works as a translator between Frontend and Models structure

--- employees/serializers.py
+++ employees/serializers.py
@@ -4,4 +4,4 @@ from .models import Employee
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
-        fields = '__all__'
+        fields = "__all__"
